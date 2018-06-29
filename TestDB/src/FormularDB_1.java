4b5145d8eaf3cc8e180571b99197b34a614fcab1
daniel <daniel@daniel-Aspire-X3995>
2018-06-29 11:16:17 +0200

    initial commit 29.06.18

---
+++ TestDB/src/FormularDB_1.java
@@ -0,0 +1,66 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+
+public class FormularDB_1 extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtTxtname;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FormularDB_1 frame = new FormularDB_1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FormularDB_1() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtTxtname = new JTextField();
+		txtTxtname.setText("txtName");
+		txtTxtname.setBounds(25, 164, 114, 19);
+		contentPane.add(txtTxtname);
+		txtTxtname.setColumns(10);
+		
+		JButton btnEinfuegen = new JButton("einfügen");
+		btnEinfuegen.setBounds(162, 161, 117, 25);
+		contentPane.add(btnEinfuegen);
+		
+		JButton btnLesen = new JButton("lesen");
+		btnLesen.setBounds(311, 161, 117, 25);
+		contentPane.add(btnLesen);
+		
+		JList listNamen = new JList();
+		listNamen.setBounds(387, 61, -145, 35);
+		contentPane.add(listNamen);
+		
+		JComboBox comboBoxNamen = new JComboBox();
+		comboBoxNamen.setBounds(12, 27, 159, 24);
+		contentPane.add(comboBoxNamen);
+	}
+}

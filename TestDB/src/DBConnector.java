f75fbb1233b4d5bab14c1d2b597501926977cd92
daniel <daniel@daniel-Aspire-X3995>
2018-06-29 14:13:33 +0200

    mit liste

--- TestDB/src/DBConnector.java
+++ TestDB/src/DBConnector.java
@@ -1,6 +1,19 @@
+import java.awt.List;
 
 public class DBConnector {
+		private List listemitNamen = new List();
+		
 		DBConnector(){
+
+		}
+		
+		public void addDaten(String name) {
+			listemitNamen.add(name);
+		}
+		
+		public String getDaten() {
+			String name = "";
 			
+			return name;
 		}
 }
